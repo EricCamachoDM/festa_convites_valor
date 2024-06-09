f03bc82b93bb91f7361fe3360f205c212fd908f4
EricCamachoDM <camacho.demolay@gmail.com>
2024-06-08 21:47:26 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
-# Função para calcular o custo total dos convites
-def calcular_custo_total(convidados, convite0, convite50, convite80, convite100, valor_convite):
-    convite_restante = convidados - convite0 - convite50 - convite80 - convite100
-    custo_total = (convite0 * 0) + (convite50 * (valor_convite * 0.5)) + (convite80 * (valor_convite * 0.8)) + (convite100 * valor_convite) 
-    return custo_total, convite_restante
-
 # Interface Streamlit
 st.title("Simulação da Festa de Aniversário")
+
+# Função para calcular o custo total dos convites
+def calcular_custo_total(convidados, convite0, convite50, convite80, valor_convite):
+    convite100 = convidados - convite0 - convite50 - convite80
+    custo_total = (convite0 * 0) + (convite50 * (valor_convite * 0.5)) + (convite80 * (valor_convite * 0.8)) + (convite100 * valor_convite)
+    convite_restante = convidados - convite0 - convite50 - convite80
+    return custo_total, convite_restante
 
 # Barras dinâmicas (sliders) para os valores de entrada
 convidados = st.slider("Número de Convidados:", 90, 140, step=1)
@@ -16,12 +17,12 @@ convite50_max = max(0, convidados - convite0)
 convite50 = st.slider("Número de Convites com 50% de Desconto:", 0, convite50_max, step=1)
 convite80_max = max(0, convidados - convite0 - convite50)
 convite80 = st.slider("Número de Convites com 20% de Desconto:", 0, convite80_max, step=1)
-convite100_max = max(0, convidados - convite0 - convite50 - convite80)
-convite100 = st.slider("Número de Convites Pagos:", 0, convite100_max, step=1)
-valor_convite = st.slider("Valor do Convite:", 90.0, 150.0, step=10.0)
+
+# Valor do Convite
+valor_convite = st.slider("Valor do Convite:", 90.0, 150.0, step=0.01)
 
 # Calcular o custo total dos convites e número de convites restantes
-custo_total, convite_restante = calcular_custo_total(convidados, convite0, convite50, convite80, convite100, valor_convite)
+custo_total, convite_restante = calcular_custo_total(convidados, convite0, convite50, convite80, valor_convite)
 
 # Mostrar o valor total dos convites e o número de convites restantes
 st.write(f"O custo total dos convites é: R${custo_total:.2f}")
