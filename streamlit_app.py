e3fb695062e6946a1cbb7a05b24094832b80025a
EricCamachoDM <camacho.demolay@gmail.com>
2024-06-08 21:38:56 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,15 +12,17 @@ st.title("Simulação da Festa de Aniversário")
 # Barras dinâmicas (sliders) para os valores de entrada
 convidados = st.slider("Número de Convidados:", 90, 140, step=1)
 convite0 = st.slider("Número de Convites Gratuitos:", 0, convidados, step=1)
-convite50 = st.slider("Número de Convites com 50% de Desconto:", 0, convidados - convite0, step=1)
+convite50_max = max(0, convidados - convite0)
+convite50 = st.slider("Número de Convites com 50% de Desconto:", 0, convite50_max, step=1)
 convite80_max = max(0, convidados - convite0 - convite50)
 convite80 = st.slider("Número de Convites com 20% de Desconto:", 0, convite80_max, step=1)
-convite100 = convidados - convite0 - convite50 - convite80
+convite100_max = max(0, convidados - convite0 - convite50 - convite80)
+convite100 = st.slider("Número de Convites Pagos:", 0, convite100_max, step=1)
 valor_convite = st.slider("Valor do Convite:", 90.0, 150.0, step=0.01)
 
 # Calcular o custo total dos convites e número de convites restantes
 custo_total, convite_restante = calcular_custo_total(convidados, convite0, convite50, convite80, convite100, valor_convite)
 
-# Mostrar o valor total dos convites
+# Mostrar o valor total dos convites e o número de convites restantes
 st.write(f"O custo total dos convites é: R${custo_total:.2f}")
 st.write(f"Número de Convites Restantes: {convite_restante}")
