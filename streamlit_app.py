ae749d0d5624ea2861830010fb364d8d2c604daa
EricCamachoDM <camacho.demolay@gmail.com>
2024-06-14 20:22:10 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,11 +19,18 @@ convite80_max = max(0, convidados - convite0 - convite50)
 convite80 = st.slider("Número de Convites com 20% de Desconto:", 0, convite80_max, step=1)
 
 # Valor do Convite
-valor_convite = st.slider("Valor do Convite:", 90.0, 150.0, step=10.0)
+valor_convite = st.slider("Valor do Convite:", 80.0, 150.0, step=10.0)
+
+# Campo de entrada para o Valor do Buffet
+valor_buffet = st.number_input("Valor do Buffet:", min_value=5000.0, max_value=20000.0, step=1000.0)
 
 # Calcular o custo total dos convites e número de convites restantes
 custo_total, convite_restante = calcular_custo_total(convidados, convite0, convite50, convite80, valor_convite)
 
+# Calcular o lucro ou déficit
+lucro = custo_total - valor_buffet
+
 # Mostrar o valor total dos convites e o número de convites restantes
 st.write(f"O custo total dos convites é: R${custo_total:.2f}")
 st.write(f"Convites valor integral: {convite_restante}")
+st.write(f"O lucro (ou déficit) é: R${lucro:.2f}")
